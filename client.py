65937a6e9a90ba57c87cbeb5d85bbd8cb85ddbcb
drkitty <bumblingkitty@gmail.com>
2014-07-12 02:58:19 -0700

    Add stupid sample client that does one thing

---
+++ client.py
@@ -0,0 +1,29 @@
+# encoding=utf-8
+from __future__ import unicode_literals
+
+import socket
+import time
+
+
+def mount(s, at, uuid=None, label=None, name=None):
+    for name, value in ((b'uuid', uuid), (b'label', label), (b'name', name)):
+        if value is not None:
+            value = value.encode('utf_8')
+            at = at.encode('utf_8')
+            msg = b'5:mount {0}:{1}={2} {3}:at={4} $'.format(
+                len(name + value) + 1, name, value,
+                len(b'at' + at) + 1, at)
+            print repr(msg)
+            s.sendall(msg)
+            return
+
+
+def main():
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.connect('/tmp/arise.sock')
+    mount(s, '/mnt/place', label='UFDé')
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
