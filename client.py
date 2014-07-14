8e4dc9965d77a89112873b935ca1a667933373f6
drkitty <bumblingkitty@gmail.com>
2014-07-14 13:18:07 -0700

    Use flags instead of actual commands and args

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # encoding=utf_8
 from __future__ import unicode_literals
 
+import argparse
 import socket
 from sys import argv
 from time import sleep
@@ -25,13 +26,38 @@ def send_command(s, command, **kwargs):
     s.sendall(msg)
 
 
+def receive_list(s):
+    pass
+
+
 def main():
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     s.connect('/tmp/arise.sock')
 
-    command = argv[1]
-    args = dict(arg.split('=', 1) for arg in argv[2:])
-    send_command(s, command, **args)
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', '--mount', action='store_true')
+    group.add_argument('-u', '--unmount', action='store_true')
+    group.add_argument('-a', '--automount', action='store_true')
+    group.add_argument('-s', '--show', action='store_true')
+    parser.add_argument('-i', '--uuid')
+    parser.add_argument('-l', '--label')
+    parser.add_argument('-n', '--name')
+    args = parser.parse_args()
+
+    for c in ('mount', 'unmount', 'automount', 'show'):
+        if getattr(args, c):
+            command = c
+
+    filters = {}
+    for f in ('uuid', 'label', 'name'):
+        if getattr(args, f) is not None:
+            filters[f] = getattr(args, f)
+
+    send_command(s, command, **filters)
+    if command == 'show':
+        receive_list(s)
+
 
 if __name__ == '__main__':
     main()
