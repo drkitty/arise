8ba020a10638036ad3eeb8b9fd10cea585a53fa9
drkitty <bumblingkitty@gmail.com>
2014-07-30 20:54:11 -0700

    Implement objects

--- common.py
+++ common.py
@@ -49,6 +49,8 @@ class SocketWrapper(object):
     (This class is not thread-safe, for what should be obvious reasons.)
     """
 
+    receive_message_g = None
+
     def __init__(self, sock, poller):
         self.sock = sock
         self.poller = poller
@@ -69,35 +71,7 @@ class SocketWrapper(object):
                 yield True
 
     def prepare_send_message(self, *items, **dictionary):
-        max_length = max(chain(
-            (len(item) for item in items),
-            (max(len(k), len(v)) for k, v in dictionary.iteritems())
-        ))
-
-        length_bits = 0
-        while max_length > 0:
-            max_length >>= 1
-            length_bits += 1
-        length_bytes = (length_bits + 7 + 3) / 8  # 3 reserved bits
-
-        message = bytearray(chr(length_bytes))
-
-        for item in items:
-            item = item.encode('utf_8')
-            message += encode_length(len(item), length_bytes) + item
-
-        first = True
-        for key, value in dictionary.iteritems():
-            key = key.encode('utf_8')
-            value = value.encode('utf_8')
-            message += encode_length(len(key), length_bytes,
-                                     MAP if first else 0) + key
-            message += encode_length(len(value), length_bytes) + value
-            first = False
-
-        message += chr(END)
-
-        message = bytes(message)
+        message = format_message(*items, **dictionary)
 
         print 'About to send message {}'.format(repr(message))
         self.send_message_g = self.send_message_generator(message)
@@ -110,9 +84,28 @@ class SocketWrapper(object):
             del self.send_message_g
             return True
 
-    def receive_message_generator(self):
-        items = []
-        dictionary = {}
+    def receive_message_generator(self, old_g):
+        def receive_thing():
+            thing_length = bytearray()
+            for _ in self.receive_bytes(length_length, into=thing_length):
+                yield
+            thing_length, flags = decode_length(thing_length, length_length)
+
+            if flags & OBJECT:
+                self.prepare_receive_message()
+                while True:
+                    ret = self.receive_message()
+                    if ret is not None:
+                        yield ret[0], ret[1]
+                    yield
+            else:
+                thing = bytearray()
+                for _ in self.receive_bytes(thing_length, into=thing):
+                    yield
+                thing = thing.decode('utf_8')
+                yield thing
+
+        message = ([], {})
 
         length_length = bytearray()
         for _ in self.receive_bytes(1, into=length_length):
@@ -126,46 +119,44 @@ class SocketWrapper(object):
             if first_byte[0] & MAP:
                 break
             elif first_byte[0] & END:
-                yield items, dictionary
+                self.receive_message_g = old_g
+                yield message
 
-            item_length = bytearray()
-            for _ in self.receive_bytes(length_length, into=item_length):
-                yield
-            item_length = decode_length(item_length, length_length)
-
-            item = bytearray()
-            for _ in self.receive_bytes(item_length, into=item):
-                yield
-            items.append(item.decode('utf_8'))
+            receive_thing_g = receive_thing()
+            while True:
+                ret = next(receive_thing_g)
+                if ret is not None:
+                    break
+            message[0].append(ret)
 
         while True:
             first_byte = bytearray()
             for _ in self.peek_at_bytes(1, into=first_byte):
                 yield
             if first_byte[0] & END:
-                yield items, dictionary
+                self.receive_message_g = old_g
+                yield message
 
             pair = [bytearray(), bytearray()]
             for i in range(2):
-                thing_length = bytearray()
-                for _ in self.receive_bytes(length_length, into=thing_length):
-                    yield
-                thing_length = decode_length(thing_length, length_length)
-
-                for _ in self.receive_bytes(thing_length, into=pair[i]):
-                    yield
-                pair[i] = pair[i].decode('utf_8')
-            dictionary[pair[0]] = pair[1]
+                receive_thing_g = receive_thing()
+                while True:
+                    ret = next(receive_thing_g)
+                    if ret is not None:
+                        break
+                pair[i] = ret
+            message[1][pair[0]] = pair[1]
 
     def prepare_receive_message(self):
-        self.receive_message_g = self.receive_message_generator()
+        self.receive_message_g = self.receive_message_generator(
+            self.receive_message_g)
         self.poller.extend(self.sock.fileno(), select.POLLIN)
 
     def receive_message(self):
         ret = next(self.receive_message_g)
         if ret is not None:
-            self.poller.remove(self.sock.fileno(), select.POLLIN)
-            del self.receive_message_g
+            if not self.receive_message_g:  # Outer level just finished.
+                self.poller.remove(self.sock.fileno(), select.POLLIN)
             items, dictionary = ret
             return items, dictionary
 
@@ -187,6 +178,52 @@ class SocketWrapper(object):
             yield
 
 
+def format_message(*items, **dictionary):
+    max_length = max(chain(
+        (len(item) for item in items),
+        (max(len(k), len(v)) for k, v in dictionary.iteritems())
+    ))
+
+    length_bits = 0
+    while max_length > 0:
+        max_length >>= 1
+        length_bits += 1
+    length_bytes = (length_bits + 7 + 3) / 8  # 3 reserved bits
+
+    message = bytearray(chr(length_bytes))
+
+    for item in items:
+        if isinstance(item, tuple):
+            message += encode_length(0, length_bytes, flags=OBJECT)
+            message += format_message(*item[0], **item[1])
+        else:
+            item = item.encode('utf_8')
+            message += encode_length(len(item), length_bytes) + item
+
+    first = True
+    for key, value in dictionary.iteritems():
+        if isinstance(key, tuple):
+            message += encode_length(0, length_bytes, flags=OBJECT)
+            message += format_message(*key[0], **key[1])
+        else:
+            key = key.encode('utf_8')
+            message += encode_length(len(key), length_bytes,
+                                     MAP if first else 0) + key
+
+        if isinstance(value, tuple):
+            message += encode_length(0, length_bytes, flags=OBJECT)
+            message += format_message(*value[0], **value[1])
+        else:
+            value = value.encode('utf_8')
+            message += encode_length(len(value), length_bytes) + value
+
+        first = False
+
+    message += chr(END)
+
+    return bytes(message)
+
+
 def encode_length(length, length_bytes, flags=0):
     encoded = b''
     for i in range(length_bytes - 1):
@@ -197,8 +234,9 @@ def encode_length(length, length_bytes, flags=0):
 
 
 def decode_length(encoded, length_bytes):
+    flags = encoded[0] & (OBJECT | MAP)
     length = encoded[0] & ~(OBJECT | MAP)
     for i in range(1, length_bytes):
         length <<= 8
         length += encoded[i]
-    return length
+    return length, flags
